95d316dfc567a5c02a73f40e88095976b577b8f0
Demo07_08 <phongnvph23556@fpt.edu.vn>
2023-09-29 23:37:59 +0700

    phong push

---
+++ app/src/main/java/com/example/du_an_dads/View/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.du_an_dads.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.du_an_dads.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/du_an_dads/View/User/SigninActivity.java
@@ -0,0 +1,16 @@
+package com.example.du_an_dads.View.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.du_an_dads.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
